53c027ae9e7972ff7e25972fb1ae33f432b7f22b
KarthikReddy1118 <boomireddykarthikreddy@gmail.com>
2026-09-07 22:51:36 +0530

    Create printing name how many times you need.py

---
+++ printing name how many times you need.py
@@ -0,0 +1,10 @@
+a=input("enter a name:")
+n=int(input("enter a number how many times you need to print the name:"))
+for i in range(1,n+1):
+    print(a)
+
+#if you nprint i then it will print the iteration number
+#if you print a then it will print the name how many times you need
+" that is the reason we are using the loop"
+  
+    
