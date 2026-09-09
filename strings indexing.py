74741e4f2e9463b96e4b4c72c26080d5ab769fc8
KarthikReddy1118 <boomireddykarthikreddy@gmail.com>
2026-09-09 11:25:32 +0530

    Create strings indexing.py

---
+++ strings indexing.py
@@ -0,0 +1,17 @@
+a="karthik"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+
+
+"these are positive indexing start from 0 and go to n-1"
+print(a[-1])
+print(a[-2])
+print(a[-3])
+
+'heare karthik is a string a[0] is k and 1 is a its start sorm starting to last index '
+"and negative index -1 is k and -2 is i like these it will work for positive and negative indexing"
+
