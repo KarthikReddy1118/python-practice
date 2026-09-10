3e0fa869bccebc50b86204094af9ffd6a6a7c516
KarthikReddy1118 <boomireddykarthikreddy@gmail.com>
2026-09-10 20:43:05 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,30 @@
+a=(1,2,3.5,"karthik")
+type(a)
+#properties of tuples
+#ordered
+#indexed
+#mutable not allowed
+#duplicated
+"any one satisfy order then index also posible"
+# acessing loops also posible
+for i in a:
+    print(i)
+for i in range(5):   #printing same tuple range -1 times
+
+    print(a)
+
+
+print("moodifying tuple was not posible ")
+print("like: a[0] = 10",)
+"index acessing was posible "
+print(a[3])
+"slicing also posible"
+print(a[1:4])
+print(a[1:4:2]) #last value is jumping value 
+print(a[ :3])
+print(a[0: ])
+
+
+
+
+
