98c7da503ce8382953c4d4315117054663f8980b
KarthikReddy1118 <boomireddykarthikreddy@gmail.com>
2026-09-10 21:00:49 +0530

    Create format keyword.py

---
+++ format keyword.py
@@ -0,0 +1,5 @@
+name="karthik reddy"
+print("how are you {0}? I am fine.".format(name))
+print("{0} you are handsome".format(name))
+
+#syntax was use {0} in side the double quotes and use.format(keep variable name inside that codes)
